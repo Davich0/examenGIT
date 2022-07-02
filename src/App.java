c39aecaa9033d070dcc5c0b57cf4e630f9051654
Davich0 <gamerAlejo2020@hotmail.com>
2022-07-01 22:33:45 -0500

    nuevos cambios

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola todos!");
-        System.out.println("Bye.. Byee...!");
+        System.out.println("Hola todos ...!");
+        System.out.println("Bye.. Byee.. Bye...!");
     }
 }
 
 // !David
 // ?En serio
 // * FFF
-// 
+//
