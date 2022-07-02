6f76e925e9045e53d8a5f7466cdb91b0e5e36742
Davich0 <gamerAlejo2020@hotmail.com>
2022-07-01 22:24:47 -0500

    inicio en el git

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola todos!");
+        System.out.println("Bye.. Byee...!");
+    }
+}
+
+// !David
+// ?En serio
+// * FFF
+// 
